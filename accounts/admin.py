24d2e99a74fb4afa54a97d805d2329e6d7a61152
Musthak <musthakcool55@gmail.com>
2026-01-28 14:21:48 +0530

    News App

---
+++ accounts/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ["email", "username", "age", "is_staff"]
+
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('age',)}),)
+
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('age',)}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
